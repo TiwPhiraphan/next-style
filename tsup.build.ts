8e9f7d301e4f43c52618ea3acf7d98f07ce1c478
TiwPhiraphan <tananphiraphan@outlook.com>
2026-05-20 14:37:20 +0700

    Update build script

---
+++ tsup.build.ts
@@ -0,0 +1,45 @@
+import { build, type Options } from 'tsup'
+
+const options: Options = {
+	external: ['csstype'],
+	sourcemap: false,
+	splitting: false,
+    outDir: 'dist',
+	bundle: true,
+	minify: true,
+	shims: true
+}
+
+await build({
+    ...options,
+    dts: true,
+    clean: true,
+    format: 'esm',
+    name: 'index.js',
+    entry: { index: 'src/index.ts' }
+})
+await build({
+    ...options,
+    dts: false,
+    clean: false,
+    format: 'cjs',
+    name: 'index.cjs',
+    entry: { index: 'src/index.ts' }
+})
+
+await build({
+    ...options,
+    dts: true,
+    clean: false,
+    format: 'esm',
+    name: 'index.js',
+    entry: { 'postcss-plugin/index': 'src/postcss-plugin/index.ts' }
+})
+await build({
+    ...options,
+    dts: false,
+    clean: false,
+    format: 'cjs',
+    name: 'index.cjs',
+    entry: { 'postcss-plugin/index': 'src/postcss-plugin/index.ts' }
+})
